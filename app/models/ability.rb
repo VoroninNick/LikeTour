0ac3d62bc4162e3119b4098295210e5517e8d137
Nazariy <nazariy.papizh@gmail.com>
2015-07-17 18:31:43 +0300

    ready role admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,17 +5,18 @@ class Ability
     # Define abilities for the passed in user here. For example:
 
     can :read, :all                   # allow everyone to read everything
-    # if user && user.nazariy?
-    #   can :access, :rails_admin       # only allow admin users to access Rails Admin
-    #   can :dashboard                  # allow access to dashboard
-    #   if user.role? :superadmin
-    #     can :manage, :all             # allow superadmins to do anything
-    #   elsif user.role? :manager
-    #     can :manage, [User, Product]  # allow managers to do anything to products and users
-    #   elsif user.role? :sales
-    #     can :update, Product, :hidden => false  # allow sales to only update visible products
-    #   end
-    # end
+    if user && user.nazariy?
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :dashboard                  # allow access to dashboard
+      if user.nazariy?
+        can :manage, :all             # allow superadmins to do anything
+      end
+      # elsif user.role? :manager
+      #   can :manage, [User, Product]  # allow managers to do anything to products and users
+      # elsif user.role? :sales
+      #   can :update, Product, :hidden => false  # allow sales to only update visible products
+      # end
+    end
     #   user ||= User.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
